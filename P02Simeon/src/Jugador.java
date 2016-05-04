f6a58a2655c785413470da5b6bfafe8292bd5a23
xXyolo420Xx <sime@reborn.com>
2016-05-05 01:52:12 +0200

    P02

---
+++ P02Simeon/src/Jugador.java
@@ -0,0 +1,65 @@
+
+public class Jugador {
+	
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	private int puntos;
+	public Jugador() {
+
+	}
+
+	
+	//GETTERS
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public int getPuntos() {
+		return puntos;
+	}
+
+	
+	//SETTERS
+	public void setNombre(String nombre) {
+		
+		if (nombre.length()<2){
+			System.out.println("Nombre invalidos, asignando nombre genérico");
+			this.nombre = "Player";
+		}else{
+		this.nombre = nombre;
+		}
+	}
+
+	public void setApellidos(String apellidos) {
+		if (apellidos.length()<2){
+			System.out.println("Apellidos invalidos, asignando apellido genérico");
+			this.apellidos="Garcia";
+		}else{
+		this.apellidos = apellidos;
+		}
+	}
+
+	public void setEdad(int edad) {
+		if (edad<1){
+			this.edad=10;
+		}else if (edad>140){
+			this.edad=140;
+		}else
+		this.edad = edad;
+	}
+	
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+
+}
